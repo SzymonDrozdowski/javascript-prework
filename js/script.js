acc4cdce5034c58c68a9400436a6a983b4682c56
Szymon Drozdowski <szymo.dr@gmail.com>
2023-01-20 18:15:54 +0100

    add variables

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var computerMove, item, playerMove;
+computerMove = 'kamień';
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to ' + computerMove + ', to wygrywasz!');
